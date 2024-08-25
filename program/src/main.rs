057a0450391b507a83ab98fd969bc921a3a7e5ce
Cristian Strat <strat.cristian@gmail.com>
2024-08-24 23:49:06 -0700

    feat: SP1 program

---
+++ program/src/main.rs
@@ -0,0 +1,20 @@
+#![no_main]
+sp1_zkvm::entrypoint!(main);
+
+use alloy_sol_types::SolType;
+use classifier::{INPUT_HEIGHT, INPUT_WIDTH};
+use classifier_io::{generate_ipfs_cid_v0, ipfs_uri, prepare_input_image, PublicValuesStruct};
+
+pub fn main() {
+    // Preprocess input image.
+    let image_file_content = sp1_zkvm::io::read_vec();
+    let input = prepare_input_image(&image_file_content, INPUT_WIDTH, INPUT_HEIGHT)
+        .expect("prepare input image");
+    let label = classifier::predict_mlp2(input);
+    // Encode public values.
+    let public_out = PublicValuesStruct::abi_encode(&PublicValuesStruct {
+        ipfsUri: ipfs_uri(generate_ipfs_cid_v0(&image_file_content)),
+        label,
+    });
+    sp1_zkvm::io::commit_slice(&public_out);
+}

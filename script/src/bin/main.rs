04b93504aadf46bb11d632e175cfd049c218daca
Cristian Strat <strat.cristian@gmail.com>
2024-09-02 16:22:04 -0700

    feat: CLI entrypoint to generate PLONK proofs

--- script/src/bin/main.rs
+++ script/src/bin/main.rs
@@ -1,7 +1,8 @@
 use alloy_sol_types::SolType;
 use clap::{Parser, ValueEnum};
 use classifier_io::{prepare_input_image, PublicValuesStruct};
-use sp1_sdk::{ProverClient, SP1Stdin};
+use serde::{Deserialize, Serialize};
+use sp1_sdk::{HashableKey, ProverClient, SP1ProofWithPublicValues, SP1Stdin, SP1VerifyingKey};
 use tracing::instrument;
 
 /// The ELF (executable and linkable format) file for the Succinct RISC-V zkVM.
@@ -21,6 +22,7 @@ struct Args {
 enum Command {
     Execute,
     Prove,
+    Evm,
 }
 
 #[instrument(skip_all)]
@@ -38,7 +40,7 @@ fn execute(client: ProverClient, stdin: SP1Stdin, program_input: &[u8]) {
 }
 
 #[instrument(skip_all)]
-fn prove(client: ProverClient, stdin: SP1Stdin) {
+fn generate_proof(client: ProverClient, stdin: SP1Stdin) {
     // Set up the program for proving.
     let (pk, vk) = client.setup(PROGRAM_ELF);
     // Generate the proof.
@@ -53,6 +55,33 @@ fn prove(client: ProverClient, stdin: SP1Stdin) {
     // Print public values.
     let decoded = PublicValuesStruct::abi_decode(proof.public_values.as_slice(), true).unwrap();
     println!("{decoded:#?}");
+}
+
+#[instrument(skip_all)]
+fn generate_evm_proof(client: ProverClient, stdin: SP1Stdin) {
+    let (pk, vk) = client.setup(PROGRAM_ELF);
+    let proof = client
+        .prove(&pk, stdin)
+        .plonk()
+        .run()
+        .expect("failed to generate proof");
+    tracing::info!("proof generated");
+    write_plonk_test_fixture(&proof, &vk);
+}
+
+/// Write test fixture to stdout; copy it into your (Forge) project.
+fn write_plonk_test_fixture(proof: &SP1ProofWithPublicValues, vk: &SP1VerifyingKey) {
+    let bytes = proof.public_values.as_slice();
+    let PublicValuesStruct { ipfsUri, label } =
+        PublicValuesStruct::abi_decode(bytes, false).unwrap();
+    let fixture = SP1ProofFixture {
+        ipfs_uri: ipfsUri,
+        label,
+        program_vkey: vk.bytes32().to_string(),
+        public_values: format!("0x{}", hex::encode(bytes)),
+        proof: format!("0x{}", hex::encode(proof.bytes())),
+    };
+    println!("{}", serde_json::to_string_pretty(&fixture).unwrap());
 }
 
 fn main() {
@@ -73,7 +102,8 @@ fn main() {
 
     match args.command {
         Command::Execute => execute(client, stdin, &image_file_content),
-        Command::Prove => prove(client, stdin),
+        Command::Prove => generate_proof(client, stdin),
+        Command::Evm => generate_evm_proof(client, stdin),
     }
 }
 
@@ -85,4 +115,15 @@ fn get_expected_label(image_file_content: &[u8]) -> u8 {
     )
     .unwrap();
     classifier::predict_mlp(input)
+}
+
+/// A fixture that can be used to test the verification of SP1 zkVM proofs inside Solidity.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct SP1ProofFixture {
+    ipfs_uri: String,
+    label: u8,
+    program_vkey: String,
+    public_values: String,
+    proof: String,
 }
